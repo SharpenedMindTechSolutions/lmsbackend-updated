ae269306cbfe501df85879263c85782667c2f437
SharpenedMindTechSolutions <sharpenmind822@gmail.com>
2026-09-10 10:24:01 +0530

    app.listen updated

--- app.js
+++ app.js
@@ -156,11 +156,17 @@ app.get('/health', (req, res) => {
 // DATABASE
 // ----------------------
 
+
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(async () => {
     console.log('MongoDB connected ✅');
 
     await seedGamesIfEmpty();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((error) => {
     console.error('MongoDB connection failed ❌', error);
